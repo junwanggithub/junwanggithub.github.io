66aba907e4d60aee06033f6713af840a0ab82d19
juwang <juwang@aerohive.com>
2015-08-29 21:29:41 -0700

    filter added

--- js/animation.js
+++ js/animation.js
@@ -1,12 +1,44 @@
-$('.portfolio li')
-    .mouseenter(function(e){
-        $(this).find('.arrow-r').hoverFlow(e.type, {opacity:1, right:5}, 500);
-        $(this).stop().animate({'opacity':'1'}, 300).siblings().stop().animate({'opacity':'0.4'}, 500);
-    }).mouseleave(function(e){
-        $(this).find('.arrow-r').hoverFlow(e.type, {opacity:0, right:0}, 500);
-});
+(function($) {
 
-$('.portfolio').mouseleave(function(e){
-    $('.portfolio li').stop().animate({'opacity':'1'}, 500);
+//portfolio filter nav click event
+    var WJ = WJ || {};
+    WJ.$portfolioContainer = $('.portfolio');
+    WJ.$portfolioItems = WJ.$portfolioContainer.find('li');
 
-});
+    $('.categories a').click(function (e) {
+        e.preventDefault();
+        var $cloneItems = WJ.$portfolioItems.clone();
+        var $this = $(e.target);
+        var filterName = $this.data('type');
+        var $target = $cloneItems.filter(function (item) {
+            return filterName === 'all' ? true : $(this).data('type') === filterName;
+        });
+
+        WJ.$portfolioContainer.quicksand($target, {
+            adjustHeight: false,
+            adjustWidth: false,
+            duration: 450
+        });
+    });
+
+    //portfolio mouse hover
+    $('.portfolio').on('mouseenter', 'li', function(e){
+        $(this).find('.arrow-r').hoverFlow(e.type, {opacity: 1, right: 5}, 500);
+        $(this).stop().animate({'opacity': '1'}, 300).siblings().stop().animate({'opacity': '0.4'}, 500);
+    });
+    $('.portfolio').on('mouseleave', 'li', function(e){
+        $(this).find('.arrow-r').hoverFlow(e.type, {opacity: 0, right: 0}, 500);
+    });
+        //.mouseenter(function (e) {
+        //    $(this).find('.arrow-r').hoverFlow(e.type, {opacity: 1, right: 5}, 500);
+        //    $(this).stop().animate({'opacity': '1'}, 300).siblings().stop().animate({'opacity': '0.4'}, 500);
+        //}).mouseleave(function (e) {
+        //    $(this).find('.arrow-r').hoverFlow(e.type, {opacity: 0, right: 0}, 500);
+        //});
+
+    $('.portfolio').mouseleave(function (e) {
+        $('.portfolio li').stop().animate({'opacity': '1'}, 500);
+
+    });
+})($);
+
